da26765cbc033b107f12923cc1fd99cab37498fd
Amisto <amisto@yandex.ru>
2018-06-06 21:48:59 +0300

    Serial calculations to adjust problem statement - material properties and sensor position.

---
+++ butterfly_serial.cpp
@@ -0,0 +1,69 @@
+#include "butterfly.h"
+#include "phasing.c"
+#define N_C 11
+#define N_S 15
+
+int main(int argc, char** argv)
+{
+    init_from_file(argv[1]);
+    FILE* setup = fopen("setup.txt", "r");
+    if (!setup)
+    {
+        printf("No file opened\n");
+        exit(-1);
+    }
+    fscanf(setup, "%d%lf", &rays_num, &focus);
+    fclose(setup);
+
+    char ftemplate[100];
+    double c_rel = 0.8, dc = 0.01;
+    for (int c = 0; c < N_C; c++)
+    {
+        obstacles[0].c_rel = c_rel;
+
+        double shift = -175.0, ds = (350.0 - DX_SENSORS*SENSORS) /N_S;
+        for (int s = 0; s < N_S; s++)
+        {
+            sprintf(ftemplate, "c_%.2lf_shift_%.2lf", c_rel, shift);
+            for(int i=0; i<SENSORS; i++)
+            {
+                sensors[i].pos.y = Y*0.999;
+                sensors[i].pos.x = X/2 - DX_SENSORS * (SENSORS / 2 - i) + shift;
+                sensors[i].writing.clear();
+            }
+    
+            char fname[100];
+            for(int i=0; i<SENSORS; i++)
+            {
+                sprintf(fname, "%s_%03d.csv", ftemplate, i);
+                f_csv = fopen(fname, "w");
+                if (!f_csv)
+                {
+                    printf("No file opened\n");
+                    exit(-1);
+                }   
+                init_explosion(X/2 - DX_SENSORS * (SENSORS / 2 - i) + shift, Y*0.999);
+    
+                T_START = SENSORS*DX_SENSORS*1.2/C0;
+                T_FINISH = 2.2*Y/C0;
+                written = 0;
+                for(int i=0; i<SENSORS; i++)
+                    sensors[i].writing.clear();
+                nonidiocy = 1;
+                while(T_FINISH > 0) calc_a_step();
+                fclose(f_csv);
+                printf("%3dth sensor of %d calculated\n", i, SENSORS);
+                finalize();
+            }
+            phasing(ftemplate, 0.0);
+            printf("c_rel %d of %d, shift %d of %d\n", c, N_C, s, N_S);
+
+            shift += ds;
+        }
+
+        c_rel += dc;
+    }
+
+    return 0;
+}
+
---
+++ hilbertize_serial.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import re
+import png
+import math
+import csv
+import numpy as np
+from scipy.signal import hilbert
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description='Hilbertizing sensor data')
+
+parser.add_argument(
+    'files',
+    metavar='FILES',
+    nargs='+',
+    help='List of CSV files with solo sensor data'
+)
+
+parser.add_argument(
+    '-o',
+    '--output',
+    required=True,
+    metavar='FILE',
+    help='Output file name'
+)
+
+parser.add_argument(
+    '--freq_min',
+    required=True,
+    type=int,
+    metavar='FREQ_MIN',
+    help='Min range of the frequency window'
+)
+
+parser.add_argument(
+    '--freq_max',
+    required=True,
+    type=int,
+    metavar='FREQ_MAX',
+    help='Max range of the frequency window'
+
+)
+
+def interpolate(v1, v2, factor):
+    return v1*(1-factor) + v2*factor
+
+args = parser.parse_args()
+
+for one_file in args.files:
+    with open(one_file) as ff:
+        vals = [[abs(float(x)) for x in l.split()] for l in ff.readlines()]
+
+        #Narrowband filtering + Hilbert transformation
+        res_fft = []
+        for i,row in enumerate(vals):
+            #print("starting " + str(i) + " of " + str(len(vals)))
+            fft = np.fft.rfft(row)
+
+            #this is to check the spectrum and find the carrying freauency
+            plt.plot(np.abs(fft)[10:]) #near-zero freqencies somehow are exremely large
+            #plt.savefig(args.output + "_fft_" + str(i) + ".png")
+            #plt.clf()
+
+            for j,freq in enumerate(fft):
+                if not (args.freq_min <= j <= args.freq_max):
+                    fft[j] = 0
+            res_fft.append(np.abs(hilbert(np.fft.irfft(fft))))
+        plt.savefig(args.output + "_spectrum_" + ".png")
+        #results -> 255 grayscale
+        min_res_fft = min(map(min, res_fft))
+        max_res_fft = max(map(max, res_fft))
+        res_fft_color = [[int(255*(x-min_res_fft)/(max_res_fft-min_res_fft)) for x in l] for l in res_fft]
+        max_res = max(map(max, vals))
+        min_res = min(map(min, vals))
+        res_color = [[int(255*(x-min_res)/(max_res-min_res)) for x in l] for l in vals]
+
+        #printin'
+        with open(args.output+".png", 'wb') as f:
+            w = png.Writer(len(res_color[0]), len(res_color), greyscale=True)
+            w.write(f, res_color)
+        with open(args.output + "_" + str(args.freq_min) + "_" + str(args.freq_max) +"_fft.png", 'wb') as f:
+            w = png.Writer(len(res_fft_color[0]), len(res_fft_color), greyscale=True)
+            w.write(f, res_fft_color)
+
+
+        data = res_fft_color
+        h = len(data)
+        w = len(data[0])
+
+        angle_rad = 120*math.pi/180
+
+        r = h/angle_rad
+        R = r + w
+        q = r*math.cos(angle_rad/2)
+
+        nh = math.ceil(R - q)
+        nw = math.ceil(2*R*math.sin(angle_rad/2))
+
+        new_data = [[0 for x in range(nw+1)] for y in range(nh+1)]
+
+        cx = nw/2
+        cy = q
+
+        for i in range(nh):
+            for j in range(nw):
+                x = j + 0.5
+                y = i + 0.5
+                dx = x - cx
+                dy = cy + y
+   
+                d = (dx**2 + dy**2)**0.5 - r
+
+                d1 = math.floor(d)
+                d2 = math.ceil(d)
+
+                a = (0.5 + math.atan2(dx, dy)/angle_rad)*h
+
+                ray1 = math.floor(a)
+                ray2 = math.ceil(a)
+
+                if all([0 <= d1 < w, 0 <= d2 < w, 0 <= ray1 < h, 0 <= ray2 < h]):
+                    r1v = interpolate(data[ray1][d1], data[ray1][d2], math.modf(d)[0])
+                    r2v = interpolate(data[ray2][d1], data[ray2][d2], math.modf(d)[0])
+                    v = interpolate(r1v, r2v, math.modf(a)[0])
+                    new_data[i][j] = v#*v#*v
+                else:
+                    new_data[i][j] = 0
+        max_data = max(map(max, new_data))
+        min_data = min(map(min, new_data))
+        data_color = [[int(255*(x-min_data)/(max_data-min_data)) for x in l] for l in new_data]
+        with open(one_file+"_sectorized.png", 'wb') as fff:
+            w = png.Writer(nw+1, nh+1, greyscale=True)
+            w.write(fff, data_color)
+ 
