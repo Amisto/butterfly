0d5418f66cbb42d634caca309d88fff5a3e11a56
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2020-10-12 14:56:57 +0300

    Revert "Update 2_hilbertize.py"
    This reverts commit e7aaeedc956f7440faf88129818e24a39c44f645.

--- 2_hilbertize.py
+++ 2_hilbertize.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import matplotlib.pyplot as plt
 import argparse
 import os
 import re
@@ -11,16 +10,33 @@ import numpy as np
 from scipy.signal import hilbert
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-# Creating parser for powershell
 parser = argparse.ArgumentParser(description='Hilbertizing sensor data')
 
-parser.add_argument(  # creates a list of csv files
+parser.add_argument(
     'files',
     metavar='FILES',
     nargs='+',
     help='List of CSV files with solo sensor data'
 )
+
+#parser.add_argument(
+#    '--freq_min',
+#    required=True,
+#    type=int,
+#    metavar='FREQ_MIN',
+#    help='Min range of the frequency window'
+#)
+#
+#parser.add_argument(
+#    '--freq_max',
+#    required=True,
+#    type=int,
+#    metavar='FREQ_MAX',
+#    help='Max range of the frequency window'
+
+#)
 
 parser.add_argument(
     '--window',
@@ -29,42 +45,31 @@ parser.add_argument(
     metavar='WINDOW',
     help='Width of the frequency window'
 )
-args = parser.parse_args()  # 2 arguments at all
 
-
-def final(file, val, pref):
-    max_data = max(map(max, val))
-    min_data = min(map(min, val))
-    data_color = [[int(255*(x-min_data)/(max_data-min_data))
-                   for x in l] for l in val]
-    with open(file+pref, 'wb') as fpng:
-        w = png.Writer(len(val[0]), len(val), greyscale=True)
-        w.write(fpng, data_color)
-
+args = parser.parse_args()
 
 for one_file in args.files:
     with open(one_file) as fi:
-        with open(one_file+"_hilb.csv", 'w') as fo:  # TODO! FIX THE ROOTS OF SAVING FILES
+        with open(one_file+"_hilb.csv", 'w') as fo:
             vals_init = [[float(x) for x in l.split()] for l in fi.readlines()]
             vals = list(zip(*vals_init))
-            # Narrowband filtering + Hilbert transformation
+            #Narrowband filtering + Hilbert transformation
             res_fft = []
             maxf = 0
-            for i, row in enumerate(vals):
+            for i,row in enumerate(vals):
                 fft = np.fft.rfft(row)
 
-                # this is to check the spectrum and find the carrying freauency
-                # near-zero freqencies somehow are exremely large
-                plt.plot(np.abs(fft)[10:])
+                #this is to check the spectrum and find the carrying freauency
+                plt.plot(np.abs(fft)[10:]) #near-zero freqencies somehow are exremely large
                 maxf += np.abs(fft)[10:].argmax()
-
+                
                 #plt.savefig(args.output + "_fft_" + str(i) + ".png")
-                # plt.clf()
+                #plt.clf()
             mid = maxf/len(vals)
-            for i, row in enumerate(vals):
+            for i,row in enumerate(vals):
                 fft = np.fft.rfft(row)
-                for j, freq in enumerate(fft):
-                    # if not (args.freq_min <= j <= args.freq_max):
+                for j,freq in enumerate(fft):
+                    #if not (args.freq_min <= j <= args.freq_max):
                     if not (mid - args.window/2.0 <= j <= mid + args.window/2.0):
                         fft[j] = 0
                 res_fft.append(np.abs(hilbert(np.fft.irfft(fft))))
@@ -78,7 +83,7 @@ for one_file in args.files:
                 stri = ""
                 for t in r:
                     stri += str(round((t - min_data)/(max_data-min_data), 2))
-                    stri += " "
+                    stri +=" "
                 print(stri, file=fo)
 
             with open(one_file+"_HIHIHILB.csv", 'w') as fu:
@@ -86,10 +91,25 @@ for one_file in args.files:
                     stri = ""
                     for t in r:
                         stri += str(round(t, 2))
-                        stri += " "
+                        stri +=" "
                     print(stri, file=fu)
 
-            # TODO! FIX THE ROOTS OF SAVING FILES
-            final(file=one_file, val=vals_init, pref="_prehilb.png")
-            # writing final graphics
-            final(file=one_file, val=res, pref="_hilb.png")
+
+            max_datap = max(map(max, vals_init))
+            min_datap = min(map(min, vals_init))
+            datap_color = [[int(255*(x-min_datap)/(max_datap-min_datap)) for x in l] for l in vals_init]
+            with open(one_file+"_prehilb.png", 'wb') as fpng:
+                w = png.Writer(len(vals_init[0]), len(vals_init), greyscale=True)
+                w.write(fpng, datap_color)
+
+            max_data = max(map(max, res))
+            min_data = min(map(min, res))
+            print(max_data, min_data)
+            data_color = [[int(255*(x-min_data)/(max_data-min_data)) for x in l] for l in res]
+            with open(one_file+"_hilb.png", 'wb') as fpng:
+                w = png.Writer(len(res[0]), len(res), greyscale=True)
+                w.write(fpng, data_color)
+
+            #for j in range(len(values[0])):
+                #print(values[0][j][0], values[0][j][1], values[0][j][2], values[0][j][3], values[0][j][4], values[0][j][5], values[0][j][6], values[0][j][7], values[0][j][8], file=f)
+        
