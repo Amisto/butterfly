47fe455352c00367e789e04d413de2e038aea21f
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2020-10-19 19:03:43 +0200

    Цитаты Вани
    Удаляй всё, что не нужно. Ничего не потеряется, это гит(с)

--- Useless files/names.py
+++
@@ -1,6 +0,0 @@
-import os #Script to change the names of files
-import re
-for i in range(1, 10):
-    os.remove('data/baseline/Sensor{}/baseline.txt_shift_0.00_00{}.csv_spectrum_.png'.format(i, i))
-for i in range(10, 32):
-    os.remove('data/baseline/Sensor{}/baseline.txt_shift_0.00_0{}.csv_spectrum_.png'.format(i, i))
--- Useless files/run_serial.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-g++ butterfly_serial.cpp -o bds -lm -O3
-time ./bds fin_0.8_3.txt setup_3.txt 
-./hilbertize_serial.py phased_fin_0.8_3.txt_shift_*.csv -o serial_3 --freq_min 150 --freq_max 550
-time ./bds fin_0.8_5.txt setup_5.txt
-./hilbertize_serial.py phased_fin_0.8_5.txt_shift_*.csv -o serial_5 --freq_min 150 --freq_max 550
