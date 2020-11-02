b7e1b884d60b63be07b826f1d52c3a79788b0dff
Daniil Likhobaba <likhobaba.dp@phystech.edu>
2020-11-02 21:28:51 +0300

    Virtual neighbors reflection and refraction
    Made 1 function for this. There may be errors here when trying to start the program

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -174,6 +174,24 @@ int Solver::checkDots(int node) {
 
 	return encounters;
 }
+
+checkerForHandler(const std::vector<Node*> &virtual_neighbor, Node &ray, bool right_virtual_neighbor) {
+    // boolean "right_virtual_neighbor" allows to avoid code duplication
+    if (ray.getMaterial() == virtual_neighbor.getMaterial()
+        && scalar(ray.getVelocity(), virtual_neighbor.getVelocity()) > 0
+        && length(ray.getPos(), virtual_neighbor.getPos()) < 5 * MINLEN) {
+        // if a wavefront is already restored, we don't bifurcate it
+        if (!virtual_neighbor.getLeft() && right_virtual_neighbor) {
+            ray.setRight(virtual_neighbor);
+            virtual_neighbor.setLeft(ray);
+        } else if (!virtual_neighbor.getRight() && !right_virtual_neighbor) {
+            ray.setLeft(virtual_neighbor);
+            virtual_neighbor.setRight(ray);
+            virtual_neighbor.setTEncounter(INFINITY);
+        }
+    }
+}
+
 void Solver::handleReflection() {
 	double delta = 0.5;
 	for (int i = 0; i < nodesNum; i++) {
@@ -182,26 +200,46 @@ void Solver::handleReflection() {
 				Node reflected = nodes[i]->getReflected(obstacles[nodes[i]->getObstacleNumber()]);
 				Node refracted = nodes[i]->getRefracted(obstacles[nodes[i]->getObstacleNumber()]);
 
-				// real neighbors always turn to ghost ones -
-				// reflected go to the other direction, refracted are in another material
-				if(nodes[i]->getLeft()){
-					Node* left = nodes[i]->getLeft();
-					reflected.addLeftVirtualNeighbor(left);
-					refracted.addLeftVirtualNeighbor(left);
-					left->addRightVirtualNeighbor(&reflected);
-					left->addLeftVirtualNeighbor(&refracted);
-					nodes[i]->setLeft(left);
-				}
-				if(nodes[i]->getRight()){
-					Node* right = nodes[i]->getRight();
-					reflected.addRightVirtualNeighbor(right);
-					refracted.addRightVirtualNeighbor(right);
-					right->addRightVirtualNeighbor(&reflected);
-					right->addLeftVirtualNeighbor(&refracted);
-					nodes[i]->setRight(right);
-				}
+				if (reflected.getIntensity() == -1) {
+				    nodes[i]->setMarkedForTheKill(1);
+				} else {
+				    // real neighbors always turn to ghost ones -
+                    // reflected go to the other direction, refracted are in another material
+                    if (nodes[i]->getLeft()) {
+                        Node *left = nodes[i]->getLeft();
+                        reflected.addLeftVirtualNeighbor(left);
+                        refracted.addLeftVirtualNeighbor(left);
+                        left->addRightVirtualNeighbor(&reflected);
+                        left->addLeftVirtualNeighbor(&refracted);
+                        nodes[i]->setLeft(left);
+                    }
+                    if (nodes[i]->getRight()) {
+                        Node *right = nodes[i]->getRight();
+                        reflected.addRightVirtualNeighbor(right);
+                        refracted.addRightVirtualNeighbor(right);
+                        right->addRightVirtualNeighbor(&reflected);
+                        right->addLeftVirtualNeighbor(&refracted);
+                        nodes[i]->setRight(right);
+                    }
+
+                    // managing ghost neighbors is trickier
+                    // ghost neighbor turn into a real one in only one case - if they share a material id
+                    // and they have approximately coinciding velocities
+                    // and not too far away
+                    //
+                    // the algorithm is designed to restore a wavefront after reflection
+                    for (int j = 0; j < nodes[i]->neighbors_left.size(); j++) {
+                        checkerForHandler(nodes[i]->neighbors_left[j], reflected, 0);
+                        checkerForHandler(nodes[i]->neighbors_left[j], refracted, 0);
+                    }
+                    for (int j = 0; j < nodes[i]->neighbors_right.size(); j++) {
+                        checkerForHandler(nodes[i]->neighbors_right[j], reflected, 1);
+                        checkerForHandler(nodes[i]->neighbors_right[j], refracted, 1);
+                    }
+                }
 			}
 		}
 	}
-
 }
+
+
