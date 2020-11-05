80c78d4fab4782937189afb43aa3bf55f1e3b1e5
Ivan Ludvig <ivanludvigdev@gmail.com>
2020-11-05 20:57:20 +0300

    Deleting old files

--- run_full_pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-g++ 1_propagate.cpp -o 1_propagate -lm -O3
-#time ./1_propagate baseline.txt setup_base.txt
-# loading 2_hilbertize.py and loading final files to the :
-time python3 2_hilbertize.py --window 10000
-g++ 3_focus.cpp -o 3_focus -lm -O3
-time ./3_focus baseline.txt setup_base.txt
-time python3 4_sectorize.py
